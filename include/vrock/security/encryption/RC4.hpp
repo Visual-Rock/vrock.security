b2420c8fd0c974367150bee261baebebc656d2eb
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2023-01-14 14:16:29 +0100

    added RC4 docu

--- include/vrock/security/encryption/RC4.hpp
+++ include/vrock/security/encryption/RC4.hpp
@@ -3,6 +3,15 @@
 #include "vrock/utils/ByteArray.hpp"
 
 namespace vrock::security::RC4 {
+    /// @brief encrypts data using the RC4 algorithm
+    /// @param data data to encrypt
+    /// @param key key for encryption 
+    /// @return encrypted data
     std::shared_ptr<utils::ByteArray> encrypt(std::shared_ptr<utils::ByteArray> data, std::shared_ptr<utils::ByteArray> key);
+    
+    /// @brief decrypts data using the RC4 algorithm
+    /// @param data data to decrypt
+    /// @param key key for decryption 
+    /// @return decrypted data
     std::shared_ptr<utils::ByteArray> decrypt(std::shared_ptr<utils::ByteArray> data, std::shared_ptr<utils::ByteArray> key);
 }
